5a08df82e5bdf47853a646a86a9b3eb711e900ea
phumebyte <phumelele.dlamini1000@gmail.com>
2024-10-25 09:33:11 +0200

    Extended function with useful error messages

--- index.js
+++ index.js
@@ -13,14 +13,20 @@ const distanceInKm = 0; // distance (km)
 const remainingFuel = 5000; // remaining fuel (kg)
 const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
-
+//calculations
 const newDistance = distanceInKm + (velocityInKPH * timeInSeconds) //calcultes new distance
-const calculateRemainingFuel = fuelBurnRate *timeInSeconds //calculates remaining fuel
+const calculateRemainingFuel = fuelBurnRate * timeInSeconds //calculates remaining fuel
 const newVelocity = calcNewVel(accelleration, velocityInKPH, timeInSeconds) //calculates new velocity based on acceleration
 
 // Pick up an error with how the function below is called and make it robust to such errors
 calcNewVel = (vel, acc, time) => { 
-  return vel + (acc*time)
+  if (typeof vel !== 'number' || typeof acc !== 'number' || typeof time !== 'number'){
+    throw new Error('All parameters must be numbers')
+  } else if(acc <= 0 || vel <= 0 || time <= 0) {
+    throw new Error('Values cannot be less than or equal to 0')
+  }
+    vel =  vel * 1000 / 3600 // convert velocity from km/h to m/s
+    return vel + (acc * time)
 }
 
 console.log(`Corrected New Velocity: ${newVelocity} km/h`);
